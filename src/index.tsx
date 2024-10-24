d6cd481245733f3d777078f6be27c8fd414b308e
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-10-24 14:45:18 +0900

    feat: Index 파일에 설정 추가 (react-query 등)

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle, LoadFont } from './styles/GlobalStyles';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ScrollToTop from './utils/ScrollToTop';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <LoadFont />
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
+        <LoadFont />
+        <GlobalStyle />
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
