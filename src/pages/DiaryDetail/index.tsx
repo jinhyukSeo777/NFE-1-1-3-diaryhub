af55a752624e15d9bd9ed76ad62e0167861218b7
thsk3 <bumakker@gmail.com>
2024-10-25 15:06:02 +0900

    refactor: diarydetail 폴더 구조 변경 - styles.css 파일을 포함하도록 폴더를 만들어 줌

---
+++ src/pages/DiaryDetail/index.tsx
@@ -0,0 +1,28 @@
+import { useParams } from 'react-router-dom';
+import Diary from '../../components/common/Diary';
+const DiaryDetail = () => {
+  const param: { id?: string } = useParams();
+  const diaryInfo = {
+    title: '일기 제목임',
+    date: '2020-10-10',
+    imgList: [
+      'https://via.placeholder.com/300.jpg',
+      'https://via.placeholder.com/400.jpg',
+      'https://via.placeholder.com/500.jpg',
+      'https://via.placeholder.com/200x300.jpg',
+      'https://via.placeholder.com/500x300.jpg',
+    ],
+    mood: '기분',
+    weather: '날씨',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi repellendus, unde incidunt exercitationem mollitia sequi quia, neque beatae perferendis consectetur illum fuga excepturi soluta libero aliquid ea ipsa laudantium!',
+  };
+  return (
+    <div>
+      <h2>
+        <span>{param.id}</span> 번 일기 상세 페이지
+      </h2>
+      <Diary diaryInfo={diaryInfo}></Diary>
+    </div>
+  );
+};
+export default DiaryDetail;
