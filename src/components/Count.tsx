70b71a7f62da465d227f3975e8f67ec317f3d7c1
Ga-Zero <rrtu123@naver.com>
2024-10-28 14:41:09 +0900

    feat: Count 컴포넌트 props 추가

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -2,13 +2,18 @@ import stamp from '../assets/stamp.svg';
 import chat from '../assets/chat.svg';
 import { container } from '../styles/Count.css';
 
-const Likes = () => {
+interface CountProps {
+  likes: string[];
+  comments: any[];
+}
+
+const Likes = ({ likes, comments }: CountProps) => {
   return (
     <div className={container}>
-      <img src={stamp} alt="stamp" style={{ width: '24px', height: 'auto' }} />
-      16
-      <img src={chat} alt="chat" />
-      16
+      <img src={stamp} alt="stamp" style={{ width: '18px', height: 'auto' }} />
+      {likes.length}
+      <img src={chat} alt="chat" style={{ width: '18px', height: 'auto' }} />
+      {comments.length}
     </div>
   );
 };
