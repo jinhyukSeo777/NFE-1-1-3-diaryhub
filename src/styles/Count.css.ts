7e5848c9e22cf67c1a418700b12ea6b92d229b9e
Ga-Zero <rrtu123@naver.com>
2024-10-28 14:40:02 +0900

    styles: Count 컴포넌트 css 설정

--- src/styles/Count.css.ts
+++ src/styles/Count.css.ts
@@ -1,12 +1,11 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
-  width: 'fit-content',
   display: 'flex',
   alignItems: 'center',
-  padding: 10,
+  padding: 8,
   gap: 4,
   borderRadius: 30,
   boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-  fontSize: '14px',
+  fontSize: '12px',
 });
