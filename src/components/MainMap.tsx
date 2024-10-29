f1cb6ae4dba90fb73480f49f0689607915557768
Ga-Zero <rrtu123@naver.com>
2024-10-29 16:43:19 +0900

    feat: map 커스텀 이미지 마커 구현

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -1,28 +1,31 @@
-import React, { useState, useEffect } from 'react';
-
+import { useState, useEffect } from 'react';
+import * as styles from '../styles/MainMap.css';
+import backImg from '../assets/markerBack.svg';
 declare global {
   interface Window {
     kakao: any;
   }
 }
+interface MapMarker {
+  latitude: number;
+  longitude: number;
+  markers: { latitude: number; longitude: number; imageUrl: string }[];
+}
 
-export default function MainMap() {
+export default function MainMap({ latitude, longitude, markers }: MapMarker) {
   const [map, setMap] = useState<any>();
-  const [marker, setMarker] = useState<any>();
 
   // 카카오맵 불러오기
   useEffect(() => {
     window.kakao.maps.load(() => {
       const container = document.getElementById('map');
       const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(latitude, longitude),
         level: 3,
       };
 
       const newMap = new window.kakao.maps.Map(container, options);
       setMap(newMap);
-      const newMarker = new window.kakao.maps.Marker();
-      setMarker(newMarker);
 
       // 현재 위치를 가져오는 함수 호출
       navigator.geolocation.getCurrentPosition(
@@ -37,8 +40,32 @@ export default function MainMap() {
           newMap.panTo(currentPos);
 
           // 마커 설정
-          newMarker.setPosition(currentPos);
-          newMarker.setMap(newMap);
+          markers.forEach((markerData) => {
+            const markerPosition = new window.kakao.maps.LatLng(
+              markerData.latitude,
+              markerData.longitude
+            );
+
+            // 마커를 담을 div 생성
+            const markerDiv = document.createElement('div');
+            markerDiv.style.backgroundImage = `url(${backImg})`;
+            markerDiv.classList.add(styles.markerDiv);
+
+            // 커스텀 마커 이미지
+            const img = document.createElement('img');
+            img.src = markerData.imageUrl;
+            img.classList.add(styles.markerImg);
+
+            // 마커 div에 이미지 추가
+            markerDiv.appendChild(img);
+
+            // CustomOverlay 생성
+            const customOverlay = new window.kakao.maps.CustomOverlay({
+              position: markerPosition,
+              content: markerDiv,
+              map: newMap,
+            });
+          });
         },
         () => alert('위치 정보를 가져오는데 실패했습니다.'),
         {
@@ -48,7 +75,7 @@ export default function MainMap() {
         }
       );
     });
-  }, []);
+  }, [markers, latitude, longitude]);
 
   return (
     <div>
