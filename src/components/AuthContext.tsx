943549368d84baffde11a3449f31a143c0815f6e
ahnjongin <goodtime1998@naver.com>
2024-10-30 17:14:55 +0900

    Refactor: 새로고침 시 로그인 풀리는 오류 수정

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -1,26 +1,41 @@
-// AuthContext.tsx
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react';
 
 interface AuthContextType {
   isLoggedIn: boolean;
-  login: (token: string) => void; // 토큰 저장을 위한 파라미터 추가
+  login: (token: string) => void;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    // localStorage에서 로그인 상태 확인
+    return Boolean(localStorage.getItem('authToken'));
+  });
 
   const login = (token: string) => {
+    localStorage.setItem('authToken', token); // 토큰을 localStorage에 저장
     setIsLoggedIn(true);
-    localStorage.setItem('token', token); // 토큰을 로컬 스토리지에 저장
   };
 
   const logout = () => {
+    localStorage.removeItem('authToken'); // 로그아웃 시 토큰 삭제
     setIsLoggedIn(false);
-    localStorage.removeItem('token'); // 로그아웃 시 토큰 삭제
   };
+
+  // 컴포넌트가 마운트될 때 로그인 상태를 다시 설정
+  useEffect(() => {
+    if (localStorage.getItem('authToken')) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
