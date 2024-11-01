1cf47423f1bfbd6c52c987352fcd56eaf67d8566
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-01 17:26:55 +0900

    fix: craco에서 src 외부 파일 참조 가능하게 변경

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,21 @@
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const path = require('path');
 
 module.exports = {
   webpack: {
     plugins: [new VanillaExtractPlugin()],
+    configure: (webpackConfig) => {
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        alias: {
+          ...webpackConfig.resolve.alias,
+          '@vanilla-extract': path.resolve(
+            __dirname,
+            'node_modules/@vanilla-extract'
+          ),
+        },
+      };
+      return webpackConfig;
+    },
   },
 };
