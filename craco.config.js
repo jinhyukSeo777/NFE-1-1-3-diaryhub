3eca198d15342e6f2b19a9d893a9427c62b9eaa5
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-01 18:26:12 +0900

    fix: craco 설정 변경 및 vercel.json 생성

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
 
 module.exports = {
   webpack: {
     plugins: [new VanillaExtractPlugin()],
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.modules = [
+        path.resolve(__dirname, 'src'),
+        'node_modules',
+      ];
+      return webpackConfig;
+    },
   },
 };
