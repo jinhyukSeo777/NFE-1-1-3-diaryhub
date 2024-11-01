4a312c8d36ecfa7a02df94b681c638751be169f9
Ga-Zero <rrtu123@naver.com>
2024-11-01 10:03:21 +0900

    feat: 공개 다이어리 없을때 보여지는 화면 구현

--- src/components/Article/ArticleArea.tsx
+++ src/components/Article/ArticleArea.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import Article from './Article';
 import Card from '../CommonCard/Card';
 import { articleArea, btn, ul, cardContainer } from './Article.css';
+import { noteCon } from '../../pages/MyDiary/MyDiary.css';
 import { Diary } from '../../pages/Home/Home';
 import btnImg from '../../assets/btn.svg';
 import { useNavigate } from 'react-router-dom';
+import note from '../../assets/note.svg';
 interface ArticleAreaProps {
   diaries: Diary[];
 }
@@ -33,19 +35,26 @@ const ArticleArea = ({ diaries }: ArticleAreaProps) => {
           style={{ width: '50px', height: 'auto' }}
         />
       </button>
-      <ul className={ul}>
-        {windowWidth >= 950 ? (
-          diaries
-            .slice(0, 5)
-            .map((diary) => <Article key={diary._id} diary={diary} />)
-        ) : (
-          <div className={cardContainer}>
-            {diaries.slice(0, 5).map((diary) => (
-              <Card key={diary._id} diary={diary} />
-            ))}
-          </div>
-        )}
-      </ul>
+      {diaries.length > 0 ? (
+        <ul className={ul}>
+          {windowWidth >= 950 ? (
+            diaries
+              .slice(0, 5)
+              .map((diary) => <Article key={diary._id} diary={diary} />)
+          ) : (
+            <div className={cardContainer}>
+              {diaries.slice(0, 5).map((diary) => (
+                <Card key={diary._id} diary={diary} />
+              ))}
+            </div>
+          )}
+        </ul>
+      ) : (
+        <div className={noteCon}>
+          <img src={note} alt="note" />
+          <p>작성된 일기가 없습니다.</p>
+        </div>
+      )}
     </section>
   );
 };
