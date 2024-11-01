d57b69806860b37be6e8e0c42817d3295614fa23
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-01 18:45:12 +0900

    fix: vercel 배포

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,7 @@
-const path = require('path');
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
 
 module.exports = {
   webpack: {
     plugins: [new VanillaExtractPlugin()],
-    configure: (webpackConfig) => {
-      webpackConfig.resolve = {
-        ...webpackConfig.resolve,
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-        fallback: {
-          fs: false,
-          path: require.resolve('path-browserify'),
-          os: false,
-        },
-      };
-      return webpackConfig;
-    },
   },
 };
