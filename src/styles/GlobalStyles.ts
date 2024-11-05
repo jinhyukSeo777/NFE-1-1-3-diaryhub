bcb779127c4fcce8c10bd1d58bfe6fd905831647
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-05 23:30:09 +0900

    refactor: 배포 버전에서 폰트가 달라지는 문제 해결
    나눔스퀘어 네오 폰트를 외부 url이 아니라 다운 받아서 사용

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,12 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
-import { useEffect } from 'react';
 import { g1 } from '../utils/color';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'HakgyoansimNadeuri';
     src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-5@1.0/HakgyoansimNadeuriTTF-B.woff2') format('woff2');
-    font-weight: 700;
+    font-weight: normal;
     font-style: normal;
   }
 
@@ -16,7 +15,13 @@ const GlobalStyle = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
-
+  @font-face {
+    font-family: 'NanumSquareNeo';
+    src: url('/fonts/NanumSquareNeo-bRg.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+ 
   * {
     margin: 0;
     padding: 0;
@@ -31,16 +36,4 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const LoadFont = () => {
-  useEffect(() => {
-    const link = document.createElement('link');
-    link.href =
-      'https://hangeul.pstatic.net/hangeul_static/css/nanum-square-neo.css';
-    link.rel = 'stylesheet';
-    document.head.appendChild(link);
-  }, []);
-
-  return null;
-};
-
-export { GlobalStyle, LoadFont };
+export default GlobalStyle;
