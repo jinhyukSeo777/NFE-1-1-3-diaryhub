c400e1d94242a92c75fc46a6ca7e1b571a24f69c
Ga-Zero <rrtu123@naver.com>
2024-11-07 17:46:30 +0900

    Feat:framer-motion 페이지 이동 효과 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Header from './components/layout/Header';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
@@ -11,24 +17,154 @@ import { AuthProvider } from './components/AuthContext';
 import EditDiary from './pages/EditDiary/EditDiary';
 import WriterDiary from './pages/WriterDiary';
 
+const pageVariants = {
+  initial: { opacity: 0, x: -50 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 50 },
+};
+
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <AuthProvider>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/creatediary" element={<CreateDiary />} />
-          <Route path="/editdiary" element={<EditDiary />} />
-          <Route path="/mydiary" element={<MyDiary />} />
-          <Route path="/diarydetail/:id" element={<DiaryDetail />} />
-          <Route path="/writerdiary/:username" element={<WriterDiary />} />
-          <Route path="/error" element={<Error />} />
-          {/* 잘못된 URL에 대한 라우팅 처리 */}
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <Home />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <Login />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <Signup />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/creatediary"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <CreateDiary />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/editdiary"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <EditDiary />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/mydiary"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <MyDiary />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/diarydetail/:id"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <DiaryDetail />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/writerdiary/:username"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <WriterDiary />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/error"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <Error />
+                </motion.div>
+              }
+            />
+            {/* 잘못된 URL에 대한 라우팅 처리 */}
+            <Route
+              path="*"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.25 }}
+                >
+                  <Error />
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </AuthProvider>
     </>
   );
