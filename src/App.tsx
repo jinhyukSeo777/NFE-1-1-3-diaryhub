c0d2fe758702b311598858ea6cf1435297095baf
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-11 11:13:14 +0900

    refactor: lazy, suspense 추가로 번들 크기 최소화

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
+import { lazy, Suspense, ReactNode } from 'react';
 import Header from './components/layout/Header/Header';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
-import CreateDiary from './pages/CreateDiary/CreateDiary';
-import MyDiary from './pages/MyDiary/MyDiary';
-import DiaryDetail from './pages/DiaryDetail/index';
-import Error from './pages/Error/Error';
 import { AuthProvider } from './components/common/AuthContext/AuthContext';
-import EditDiary from './pages/EditDiary/EditDiary';
-import WriterDiary from './pages/WriterDiary/WriterDiary';
-import { ReactNode } from 'react';
+const Home = lazy(() => import('./pages/Home/Home'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Signup = lazy(() => import('./pages/Signup/Signup'));
+const CreateDiary = lazy(() => import('./pages/CreateDiary/CreateDiary'));
+const MyDiary = lazy(() => import('./pages/MyDiary/MyDiary'));
+const DiaryDetail = lazy(() => import('./pages/DiaryDetail/index'));
+const Error = lazy(() => import('./pages/Error/Error'));
+const EditDiary = lazy(() => import('./pages/EditDiary/EditDiary'));
+const WriterDiary = lazy(() => import('./pages/WriterDiary/WriterDiary'));
 
 interface AnimatedRouteProps {
   element: ReactNode;
@@ -34,45 +34,47 @@ function App() {
   return (
     <AuthProvider>
       <Header />
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<AnimatedRoute element={<Home />} />} />
-          <Route
-            path="/login"
-            element={<AnimatedRoute element={<Login />} />}
-          />
-          <Route
-            path="/signup"
-            element={<AnimatedRoute element={<Signup />} />}
-          />
-          <Route
-            path="/creatediary"
-            element={<AnimatedRoute element={<CreateDiary />} />}
-          />
-          <Route
-            path="/editdiary"
-            element={<AnimatedRoute element={<EditDiary />} />}
-          />
-          <Route
-            path="/mydiary"
-            element={<AnimatedRoute element={<MyDiary />} />}
-          />
-          <Route
-            path="/diarydetail/:id"
-            element={<AnimatedRoute element={<DiaryDetail />} />}
-          />
-          <Route
-            path="/writerdiary/:username"
-            element={<AnimatedRoute element={<WriterDiary />} />}
-          />
-          <Route
-            path="/error"
-            element={<AnimatedRoute element={<Error />} />}
-          />
-          {/* 잘못된 URL에 대한 라우팅 처리 */}
-          <Route path="*" element={<AnimatedRoute element={<Error />} />} />
-        </Routes>
-      </AnimatePresence>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<AnimatedRoute element={<Home />} />} />
+            <Route
+              path="/login"
+              element={<AnimatedRoute element={<Login />} />}
+            />
+            <Route
+              path="/signup"
+              element={<AnimatedRoute element={<Signup />} />}
+            />
+            <Route
+              path="/creatediary"
+              element={<AnimatedRoute element={<CreateDiary />} />}
+            />
+            <Route
+              path="/editdiary"
+              element={<AnimatedRoute element={<EditDiary />} />}
+            />
+            <Route
+              path="/mydiary"
+              element={<AnimatedRoute element={<MyDiary />} />}
+            />
+            <Route
+              path="/diarydetail/:id"
+              element={<AnimatedRoute element={<DiaryDetail />} />}
+            />
+            <Route
+              path="/writerdiary/:username"
+              element={<AnimatedRoute element={<WriterDiary />} />}
+            />
+            <Route
+              path="/error"
+              element={<AnimatedRoute element={<Error />} />}
+            />
+            {/* 잘못된 URL에 대한 라우팅 처리 */}
+            <Route path="*" element={<AnimatedRoute element={<Error />} />} />
+          </Routes>
+        </AnimatePresence>
+      </Suspense>
     </AuthProvider>
   );
 }
